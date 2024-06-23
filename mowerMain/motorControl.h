c28810386d5ae9e1cd13e6d9b0210be9a192a958
Gverox <157602937+Gverox@users.noreply.github.com>
2024-06-23 16:50:29 +0200

    Update motorControl.h

--- mowerMain/motorControl.h
+++ mowerMain/motorControl.h
@@ -1,13 +1,13 @@
 /*
 2024-04-17
 File that handles the DC-Motors for the lawnmower
-contains
-forward, backward, turnLeft, turnRight and stop
+
+This file contains code for following directions:
+forward, backward, turn left, turn right and stop
 
 The function is part of the Bachelor thesis of Marko Gvero and Cecil Knudsen
 
 */
-
 
 
 #ifndef MOTORCONTROL_H
@@ -15,25 +15,13 @@ The function is part of the Bachelor thesis of Marko Gvero and Cecil Knudsen
 
 #include <Arduino.h>
 
-void stop(byte* motorPins) {
-  digitalWrite(motorPins[0], LOW);
-  digitalWrite(motorPins[1], LOW);
-  digitalWrite(motorPins[2], LOW);
-  digitalWrite(motorPins[3], LOW);
-    //Logic Voltage
-  digitalWrite(motorPins[4], LOW); //EnA
-  digitalWrite(motorPins[5], LOW);//EnB
-}
-
 void forward(byte* motorPins) {
   digitalWrite(motorPins[0], HIGH);
   digitalWrite(motorPins[1], LOW);
   digitalWrite(motorPins[2], LOW);
   digitalWrite(motorPins[3], HIGH);
-
-  //Logic Voltage
   digitalWrite(motorPins[4], HIGH); //EnA
-  digitalWrite(motorPins[5], HIGH);//EnB
+  digitalWrite(motorPins[5], HIGH); //EnB
 }
 
 void backward(byte* motorPins) {
@@ -41,35 +29,35 @@ void backward(byte* motorPins) {
   digitalWrite(motorPins[1], HIGH);
   digitalWrite(motorPins[2], HIGH);
   digitalWrite(motorPins[3], LOW);
-    //Logic Voltage
   digitalWrite(motorPins[4], HIGH); //EnA
-  digitalWrite(motorPins[5], HIGH);//EnB
+  digitalWrite(motorPins[5], HIGH); //EnB
 }
 
-void turnLeft(byte* motorPins, int turnTime) {
+void turnLeft(byte* motorPins) {
   digitalWrite(motorPins[0], LOW);
   digitalWrite(motorPins[1], HIGH);
   digitalWrite(motorPins[2], LOW);
   digitalWrite(motorPins[3], HIGH);
-    //Logic Voltage
   digitalWrite(motorPins[4], HIGH); //EnA
-  digitalWrite(motorPins[5], HIGH);//EnB
-  delay(turnTime);
-  stop(motorPins);
+  digitalWrite(motorPins[5], HIGH); //EnB
 }
 
-void turnRight(byte* motorPins, int turnTime) {
+void turnRight(byte* motorPins) {
   digitalWrite(motorPins[0], HIGH);
   digitalWrite(motorPins[1], LOW);
-  digitalWrite(motorPins[2], LOW);
-  digitalWrite(motorPins[3], HIGH);
-    //Logic Voltage
+  digitalWrite(motorPins[2], HIGH);
+  digitalWrite(motorPins[3], LOW);
   digitalWrite(motorPins[4], HIGH); //EnA
-  digitalWrite(motorPins[5], HIGH);//EnB
-  delay(turnTime);
-  stop(motorPins);
+  digitalWrite(motorPins[5], HIGH); //EnB
 }
 
+void stop(byte motorPins[]) {
+  for (byte i = 0; i < 4; i++) {
+    digitalWrite(motorPins[i], LOW);
+  }
+  analogWrite(motorPins[4], 0);
+  analogWrite(motorPins[5], 0);
+}
 
+#endif
 
-#endif 
